c4f11e7035573f6113aa660a04274d0949267b63
Zara <2019zding@dwight.edu>
2017-12-28 10:39:37 -0500

    API added link

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  
+  get 'ask', to: 'map#ask'
 
   get 'show', to: 'map#show'
 
-  get 'about' => 'static_pages#about'
+  get 'home', to: 'static_pages#home'
 
-  get 'contact' => 'static_pages#contact'
+  get 'about', to: 'static_pages#about'
 
-  get 'personalInterests' => 'static_pages#personalInterests'
+  get 'contact', to: 'static_pages#contact'
+
+  get 'personalInterests', to: 'static_pages#personalInterests'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
